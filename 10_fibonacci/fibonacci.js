b6e010595547abf8100d7bf810c70b303d63670f
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-07-15 15:07:12 +0300

    dfdj

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+function fibonacci(countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
 
-};
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
+
+}
+
+console.log(fibonacci(6))
 
 // Do not edit below this line
 module.exports = fibonacci;
